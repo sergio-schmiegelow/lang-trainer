4ec390d1f885a54b4b216db732ad8ab8179cd1d5
sergio-schmiegelow <sergio.schmiegelow@gmail.com>
2024-08-27 21:41:18 -0400

    templateQueryGeneratorClass

--- lang_training.py
+++ lang_training.py
@@ -4,33 +4,52 @@ import random
 from conjugation import regVerbesClass
 QUERY_SPACE = '___'
 #-------------------------------------------------------------------------
-def getFillers(phrase):
-    matches = []
-    for res in re.finditer('{(.+?)}', phrase):
-        #print(f'DEBUG - res = {res}')
-        matches.append([res.start(), res.end()])
-    return matches
+class templateQueryGeneratorClass:
+    def __init__(self, filename):
+        self.phrases = []
+        with open(filename) as fp:
+            lines = fp.readlines()
+            for line in lines:
+                line = line.strip()
+                if len(line.strip()) > 0:
+                    self.phrases.append(line.strip())
+    #-------------------------------------------------------------------------
+    def getFillers(self, phrase):
+        matches = []
+        for res in re.finditer('{(.+?)}', phrase):
+            #print(f'DEBUG - res = {res}')
+            matches.append([res.start(), res.end()])
+        return matches
+    #-------------------------------------------------------------------------
+    def getDefinedQuery(self, phrase, fillers, index):
+        #extract the filler
+        start, end = fillers[index]
+        answer = phrase[start + 1: end - 1]
+        #replace the filler
+        queryPhrase = phrase[:start] + QUERY_SPACE + phrase[end:]
+        queryPhrase = queryPhrase.replace('{', '')
+        queryPhrase = queryPhrase.replace('}', '')
+        if queryPhrase.startswith('('):
+            parClose = queryPhrase.index(')')
+            query = queryPhrase[1: parClose] + '\n' + queryPhrase[parClose + 1:].strip()
+        else:
+            query = queryPhrase
+        return query, [answer.lower()], ''
+    #-------------------------------------------------------------------------
+    def getQuery(self):
+        phrase = random.choice(self.phrases)
+        fillers = self.getFillers(phrase)
+        index = random.choice(range(len(fillers)))
+        return self.getDefinedQuery(phrase, fillers, index)
 #-------------------------------------------------------------------------
-def buildQuery(phrase, fillers, index):
-    #extract the filler
-    start, end = fillers[index]
-    word = phrase[start + 1: end - 1]
-    #replace the filler
-    queryPhrase = phrase[:start] + QUERY_SPACE + phrase[end:]
-    queryPhrase = queryPhrase.replace('{', '')
-    queryPhrase = queryPhrase.replace('}', '')
-    return queryPhrase, word
-#-------------------------------------------------------------------------
-def generateQuery(phrases):
-    phrase = random.choice(phrases)
-    fillers = getFillers(phrase)
-    index = random.choice(range(len(fillers)))
-    return buildQuery(phrase, fillers, index)
-#-------------------------------------------------------------------------
-verbGen = regVerbesClass()
+generators = []
+generators.append(regVerbesClass())
+generators.append(templateQueryGeneratorClass('interrogatif.ltr'))
+generators.append(templateQueryGeneratorClass('famille.ltr'))
+generators.append(templateQueryGeneratorClass('genres.ltr'))
 hits = 0
 misses = 0
-query, answers, hint = verbGen.getQuery()
+query, answers, hint = random.choice(generators).getQuery()
 while True:
     print('----------------------------------------------------')
     print(query)
@@ -38,7 +57,7 @@ while True:
     if cand.strip().lower() in answers:
         print('Correct!')
         hits += 1
-        query, answers, hint = verbGen.getQuery()
+        query, answers, hint = random.choice(generators).getQuery()
     else: 
         print(r'\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\')
         print(f'Faux. Le bon est "{answers[0]}"')
