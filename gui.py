6e4a9654a7bceef8b75353cb8e6adf115993ef18
sergio-schmiegelow <sergio.schmiegelow@gmail.com>
2024-09-28 13:59:19 -0400

    -First working GUI

---
+++ gui.py
@@ -0,0 +1,92 @@
+import PyQt5
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QTimer
+import sys
+
+from lang_training import templateQueryGeneratorClass,  queryGeneratorClass
+from conjugation import regVerbesClass
+#-------------------------------------------------------------------------
+class mainApp(QWidget):
+    #---------------------------------------------------------------------
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Formation en langue française")
+        #self.setGeometry(100, 100, 800, 600)
+        self.mainLayout = QVBoxLayout()
+        self.setLayout(self.mainLayout)
+        self.statementLabel = QLabel('statement')
+        self.mainLayout.addWidget(self.statementLabel)
+
+        self.queryLayout = QHBoxLayout()
+        self.prePhraseLabel = QLabel('preAnswer')
+        self.answerInput = QLineEdit('')
+        self.answerInput.returnPressed.connect(self.validadeAnswer)
+        self.postPhraseLabel = QLabel('postAnswer')
+        self.validadeButton = QPushButton('Valider')
+        self.validadeButton.clicked.connect(self.validadeAnswer)
+        self.queryLayout.addWidget(self.prePhraseLabel)
+        self.queryLayout.addWidget(self.answerInput)
+        self.queryLayout.addWidget(self.postPhraseLabel)
+        self.queryLayout.addWidget(self.validadeButton)
+        self.mainLayout.addLayout(self.queryLayout)
+
+        self.validationLayout = QVBoxLayout()
+        self.validationLabel = QLabel('')
+        self.validationLayout.addWidget(self.validationLabel)
+        self.mainLayout.addLayout(self.validationLayout)
+
+        self.scoreLayout = QVBoxLayout()
+        self.hitsLabel   = QLabel('Succès: 0')
+        self.missesLabel = QLabel('Erreurs: 0')
+        self.scoreLayout.addWidget(self.hitsLabel)
+        self.scoreLayout.addWidget(self.missesLabel)
+        self.mainLayout.addLayout(self.scoreLayout)
+
+        self.generator = queryGeneratorClass()
+        self.generator.addSource(regVerbesClass(),                                  1)
+        self.generator.addSource(templateQueryGeneratorClass('interrogatif.ltr'),   1)
+        self.generator.addSource(templateQueryGeneratorClass('famille.ltr'),        1)
+        self.generator.addSource(templateQueryGeneratorClass('genres.ltr'),         1)
+        self.hits = 0
+        self.misses = 0
+        self.setQuery()
+    #---------------------------------------------------------------------
+    def setQuery(self):
+        statement, prePhrase, self.answers, postPhrase, hint = self.generator.getQuery()
+        self.statementLabel.setText(statement)
+        self.prePhraseLabel.setText(prePhrase)
+        self.answerInput.setText('')
+        self.postPhraseLabel.setText(postPhrase)
+        self.validationLabel.setText('')
+    #---------------------------------------------------------------------
+    def validadeAnswer(self):
+        typedAnswer = self.answerInput.text().strip()
+        if typedAnswer in self.answers:
+            self.reportHit()
+            self.validationLabel.setText('Correct!')
+            self.validationLabel.setStyleSheet("color: blue;")
+            QTimer.singleShot(1000, self.setQuery)
+        else:
+            self.reportMiss()
+            self.validationLabel.setText(f'Faux. Le bon est "{self.answers[0]}"')
+            self.validationLabel.setStyleSheet("color: red;")
+    #---------------------------------------------------------------------
+    def reportHit(self):
+        self.hits += 1
+        self.hitsLabel.setText(f'Succès: {self.hits}')
+    #---------------------------------------------------------------------
+    def reportMiss(self):
+        self.misses += 1
+        self.missesLabel.setText(f'Erreurs: {self.misses}')
+#-------------------------------------------------------------------------
+def main():
+    app = QApplication(sys.argv)
+    window = mainApp()
+    window.show()
+    sys.exit(app.exec_())
+#-------------------------------------------------------------------------
+if __name__ == "__main__":
+    x='asd'
+    
+    main()
